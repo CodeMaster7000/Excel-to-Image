7fa6c42617c5927bacb36b2b15c833fbc95c2842
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-08-11 15:06:38 +0530

    Update Excel2PNG.py

--- src/Excel2PNG.py
+++ src/Excel2PNG.py
@@ -4,4 +4,4 @@ imgOptions.setSaveFormat(SaveFormat.SVG)
 sheet = workbook.getWorksheets().get(0)
 sr = SheetRender(sheet, imgOptions)
 for i in range(0, sr.getPageCount()):
-	sr.toImage(i, "Worksheet2PNG-output%s" %(j) + ".png") # Converts sheet to PNG image
+	sr.toImage(i, "Worksheet2PNG-output%s" %(j) + ".png") # Converts each sheet to a PNG image.
